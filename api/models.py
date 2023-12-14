a61e032bc2d1a07348265966788ad96384d4db3f
nassimdammak <nassim.dammak@ynov.com>
2023-12-14 13:12:46 +0100

    classes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Concessionnaire(models.Model):
+    nom = models.CharField(max_length=255)
+    numero_siret = models.PositiveIntegerField()
+    code_postal = models.CharField(max_length=5)
+
+class Voiture(models.Model):
+    marque = models.CharField(max_length=255)
+    modele = models.CharField(max_length=255)
+    chevaux = models.PositiveIntegerField()
+    concessionnaire = models.ForeignKey(Concessionnaire, on_delete=models.CASCADE)
